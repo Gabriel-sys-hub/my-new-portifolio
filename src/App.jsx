8b05de228b05f0c703122ee51acf2631a8c332cb
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2023-01-20 14:59:09 -0300

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/header/header';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Header></Header>
+    </div>
+  )
+}
+
+export default App
