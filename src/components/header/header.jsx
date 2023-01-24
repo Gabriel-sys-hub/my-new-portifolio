0bff12d433fcafde0ccfed5c455d2edd5ed67f7a
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-24 18:58:53 -0300

    creating header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,21 @@
-
+import  './header.css'
 import React from 'react'
+import Button from '../button/button'
 
 export default function header() {
   return (
-    <div>
-      <h1>header</h1>
+    <div className='header_div'>
+       <div className='octagon'>
+
+        <h4>G</h4>
+       </div>
+      <div className='header_content'>
+      <h5>Sobre</h5>
+      <h5>Experiencia</h5>
+      <h5>Trabalho</h5>
+      <h5>Contato</h5>
+      <Button name="Resume"></Button>
+      </div>
     </div>
   )
 }
