cfc997694e9097be98a75a5dc669c4fd5191e391
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-24 19:22:54 -0300

    header animation functions / css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,37 @@
 import  './header.css'
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Button from '../button/button'
 
 export default function header() {
-  return (
-    <div className='header_div'>
-       <div className='octagon'>
 
-        <h4>G</h4>
-       </div>
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) { 
+        setShow(true); 
+      } else {
+        setShow(false);  
+      }
+      setLastScrollY(window.scrollY); 
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
+  return (
+    <div className={`active ${show && 'hidden'}`}>
+
+    <div className='header_div'>
       <div className='header_content'>
       <h5>Sobre</h5>
       <h5>Experiencia</h5>
@@ -16,6 +39,7 @@ export default function header() {
       <h5>Contato</h5>
       <Button name="Resume"></Button>
       </div>
+    </div>
     </div>
   )
 }
