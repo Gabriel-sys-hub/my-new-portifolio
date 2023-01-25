34b2edbb10197748f31ccad359d5989f11731a84
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-24 21:20:05 -0300

    feature: adding first content, as introducing my self

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import './App.css'
+import Content from "./components/content/content";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-     <Header></Header>
-     <div className='testing_content'><h1>hey</h1></div>
-     <Footer></Footer>
+      <Header></Header>
+      <Content></Content>
+      <Content></Content>
+      <Content></Content>
+      <Footer></Footer>
     </div>
-  )
+  );
 }
 
 export default App
