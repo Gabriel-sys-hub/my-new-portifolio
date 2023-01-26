3a816bfa5ae421aa13542d3892939faefdb45e5a
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-26 18:05:16 -0300

    feature: adding github profile content

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Content from "./components/content/content";
 import "./App.css";
+import Github from "./components/github/github";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Content></Content>
-      <Content></Content>
-      <Content></Content>
+      <Github></Github>
+
       <Footer></Footer>
     </div>
   );
