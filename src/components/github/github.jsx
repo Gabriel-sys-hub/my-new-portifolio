5e4ef3a43f6898d78376d7d504527bba41404b18
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-27 09:04:21 -0300

    feature: github user info stacks

--- src/components/github/github.jsx
+++ src/components/github/github.jsx
@@ -1,14 +1,15 @@
 import { React, useEffect, useState } from "react";
+import { DiJsBadge, DiReact } from "react-icons/di";
+import { SiFlutter, SiDart, SiTypescript } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
 import axios from "axios";
 import "./github.css";
 
 export default function Github() {
   const [userGit, setUserGit] = useState([]);
-  const [userGitRepositories, setUserGitRepositories] = useState([]);
 
   useEffect(() => {
     getGitHubUserData();
-    getGitHubRepositoriesData();
   }, []);
 
   function getGitHubUserData() {
@@ -16,26 +17,26 @@ export default function Github() {
       setUserGit(result.data);
     });
   }
-  function getGitHubRepositoriesData() {
-    axios
-      .get("https://api.github.com/users/Gabriel-sys-hub/repos")
-      .then((result) => {
-        setUserGitRepositories(result.data);
-      });
-  }
-
   return (
     <div className="github-content">
       <div className="user-card">
         <div className="user-image">
           <img src={userGit.avatar_url} />
         </div>
-        <div>
-          <h2>{userGit.name}</h2>
-          <h4 className="user-bio">{userGit.bio}</h4>
-          <h4 className="user-bio">{userGit.location}</h4>
-
-          <h5 className="user-bio-github">{userGit.html_url}</h5>
+        <div className="user-bio-github-box">
+          <div>
+            <h2 className="user-bio">{userGit.bio}</h2>
+            <h4 className="user-bio">{userGit.location}</h4>
+            <h5 className="user-bio-github">{userGit.html_url}</h5>
+          </div>
+          <div className="user-stacks">
+            <SiDart className="user-stacks-icons"></SiDart>
+            <FaNode className="user-stacks-icons"></FaNode>
+            <DiJsBadge className="user-stacks-icons"></DiJsBadge>
+            <DiReact className="user-stacks-icons"></DiReact>
+            <SiFlutter className="user-stacks-icons"></SiFlutter>
+            <SiTypescript className="user-stacks-icons"></SiTypescript>
+          </div>
         </div>
       </div>
     </div>
