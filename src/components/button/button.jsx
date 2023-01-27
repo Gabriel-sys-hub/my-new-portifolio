219f058b02576f3f6afacd98ce70828a6eeb82eb
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-27 13:00:45 -0300

    feature: changing button structure

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,9 +3,14 @@ import './button.css'
 export default function Button(props) {
     const name = props.name;
   return (
-    <div className='button-glow'>
-
-        <h5 className='button-name'>{name}</h5>
-    </div>
-  )
+    <a href={props.href}>
+      <button
+        disabled={props.disabled}
+        onClick={props.onClick}
+        className="button-glow"
+      >
+        <h5 className="button-name">{name}</h5>
+      </button>
+    </a>
+  );
 }
