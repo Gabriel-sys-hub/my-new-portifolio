a14f89606134906bf28b035b8ad1ada0c748f3e9
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-27 13:49:06 -0300

    feature: creating timeline item.

---
+++ src/components/timeline-item/timelineitem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./timelineitem.css";
+
+export default function Timelineitem() {
+  return (
+    <>
+      <div className="timeline-item">
+        <div className="timeline-item-content">
+          <h3>Desrotulando</h3>
+          <h5>Desenvolvedor de Software Junior</h5>
+          <h5>15/12/2021 até 5/12/2022</h5>
+        </div>
+      </div>
+      <div className="timeline-item">
+        <div className="timeline-item-content">
+          <h3>Teste</h3>
+          <h5>Desenvolvedor de Software Junior</h5>
+          <h5>15/12/2021 até 5/12/2022</h5>
+        </div>
+      </div>
+    </>
+  );
+}
