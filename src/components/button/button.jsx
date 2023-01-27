08fc8f90426187ca60ff5a202162d54db45f5452
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-01-27 14:59:11 -0300

    feature: button and timeline

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@ import './button.css'
 export default function Button(props) {
     const name = props.name;
   return (
-    <a href={props.href}>
+    <a href={props.href} target="_blank" rel="noreferrer">
       <button
         disabled={props.disabled}
         onClick={props.onClick}
--- src/components/timeline-item/timelineitem.jsx
+++ src/components/timeline-item/timelineitem.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import "./timelineitem.css";
 
 export default function Timelineitem() {
-  return (
-    <>
-      <div className="timeline-item">
-        <div className="timeline-item-content">
-          <h3>Desrotulando</h3>
-          <h5>Desenvolvedor de Software Junior</h5>
-          <h5>15/12/2021 até 5/12/2022</h5>
-        </div>
+  const jobs = [
+    {
+      empresa: "Desrotulando",
+      cargo: "Desenvolvedor de Software Junior",
+      tempo: "15/12/2021 até 5/12/2022",
+    },
+    {
+      empresa: "Desrotulando",
+      cargo: "Desenvolvedor de Software Junior",
+      tempo: "15/12/2021 até 5/12/2022",
+    },
+    {
+      empresa: "Desrotulando",
+      cargo: "Desenvolvedor de Software Junior",
+      tempo: "15/12/2021 até 5/12/2022",
+    },
+  ];
+
+  return jobs.map((eachitem) => (
+    <div className="timeline-item">
+      <div className="timeline-item-content">
+        <h3>{eachitem.empresa}</h3>
+        <h5>{eachitem.cargo}</h5>
+        <h5>{eachitem.tempo}</h5>
       </div>
-      <div className="timeline-item">
-        <div className="timeline-item-content">
-          <h3>Teste</h3>
-          <h5>Desenvolvedor de Software Junior</h5>
-          <h5>15/12/2021 até 5/12/2022</h5>
-        </div>
-      </div>
-    </>
-  );
+    </div>
+  ));
 }
